1340125658ab97fdaf7046ecb04de864c6d106cf
jdh9862 <jdh9862@gmail.com>
2022-04-20 10:11:09 -0700

    Implement Updater

--- main.cpp
+++ main.cpp
@@ -75,33 +75,33 @@ struct Comparator                                //4
     }
 };
 
-//struct U
-//{
-//    float <#name1#> { 0 }, <#name2#> { 0 };
+struct U
+{
+    float first { 0 }, second { 0 };
 //    <#returnType#> <#memberFunction#>(<#type name#>* <#updatedValue#>)      //12
 //    {
 //
 //    }
-//};
+};
 
-//struct <#structname2#>
-//{
-//    static <#returntype#> <#staticFunctionA#>(U* that, <#type name#>* <#updatedValue#> )        //10
-//    {
-//        std::cout << "U's <#name1#> value: " << that-><#name1#> << std::endl;
-//        that-><#name1#> = <#updatedValue#>;
-//        std::cout << "U's <#name1#> updated value: " << that-><#name1#> << std::endl;
-//        while( std::abs(that-><#name2#> - that-><#name1#>) > 0.001f )
-//        {
-//            /*
-//             write something that makes the distance between that-><#name2#> and that-><#name1#> get smaller
-//             */
-//            that-><#name2#> += ;
-//        }
-//        std::cout << "U's <#name2#> updated value: " << that-><#name2#> << std::endl;
-//        return that-><#name2#> * that-><#name1#>;
-//    }
-//};
+struct Updater
+{
+    static float update(U* that, float* updatedValue )        //10
+    {
+        std::cout << "U's first value: " << that->first << std::endl;
+        that->first = *updatedValue;
+        std::cout << "U's first updated value: " << that->first << std::endl;
+        while( std::abs(that->second - that->first) > 0.001f )
+        {
+            /*
+             write something that makes the distance between that->second and that->first get smaller
+             */
+            that->second += 0.00001f;
+        }
+        std::cout << "U's second updated value: " << that->second << std::endl;
+        return that->second * that->first;
+    }
+};
         
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
@@ -126,10 +126,10 @@ int main()
     auto* smaller = f.compare(&t1 , &t2);                              //8
     std::cout << "the smaller one is << " << smaller->name << std::endl; //9
     
-//    U <#name3#>;
-//    float updatedValue = 5.f;
-//    std::cout << "[static func] <#name3#>'s multiplied values: " << <#structname2#>::<#staticFunctionA#>( , ) << std::endl;                  //11
-//
+    U u1;
+    float updatedValue = 5.f;
+    std::cout << "[static func] u1's multiplied values: " << Updater::update(&u1, &updatedValue) << std::endl;                  //11
+
 //    U <#name4#>;
 //    std::cout << "[member func] <#name4#>'s multiplied values: " << <#name4#>.<#memberFunction#>( &updatedValue ) << std::endl;
 }
